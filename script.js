d5451a3cbd68e9e5a53a52562be63940bb1c1cc7
HukoJlauII <polosatay_zebra@mail.ru>
2021-12-05 19:28:47 +0300

    final commit

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 let menu_btn = document.getElementById('menubtn')
 let content = document.getElementById('main')
-// let home = document.getElementById('home')
-// let about = document.getElementById('about')
-// let services = document.getElementById('services')
-// let contacts = document.getElementById('contacts')
 let menu=document.getElementById('navbarmenu')
 let footer =document.getElementById('footbarmenu')
 let access = true;
@@ -24,23 +20,18 @@ function change_icon(path){
     setTimeout(function (){menu_btn.style.opacity = '1'}, 1000);
 }
 
-// function waiting(){
-//     access = false;
-//     setTimeout(function (){access = true}, 2100);
-// }
-
 function swipe(){
     if (access){
         if (menu_btn.src.includes('/images/menu')) {
             change_icon('./images/x_button.svg');
             setTimeout(animation_add_menu, 1000);
             menu.style.opacity='1'
-            // waiting();
+
         } else {
             change_icon('/images/menu.png');
             menu.style.opacity='0'
             setTimeout(animation_add_content, 1000);
-            // waiting();
+
         }
     }
 }
